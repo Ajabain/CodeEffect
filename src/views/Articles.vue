c109a770ab6715ab7f4e5e5a5e48be6aa805eafc
Ajabain <mohammedyahya53@gmail.com>
2023-02-08 12:15:18 +0200

    add

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-[#015C89] pt-[60px] h-screen">
-        <h1 class="text-[70px] font-bold text-[#CCCCCC] text-center">Header Text</h1>
+        <h1 class="text-[70px] font-bold text-[#CCCCCC] text-center">Header Text </h1>
         <p class="text-[25px] font-normal text-[#CCCCCC] text-center mb-[50px]">Lorem ipsim pellentesque volutpat Alquet Ultrucies</p>
         <div class="px-[50px]  flex justify-between items-center pb-[30px]">
             <ArticalCard/>
